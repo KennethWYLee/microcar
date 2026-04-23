07107957bd6184b41ad3ba7f404202e51245c08b
KennethWYLee <kenneth.wy.lee21@gmail.com>
2026-04-23 11:42:01 +0800

    Move topic navigation below brand

--- tools/build_case_topic_pages.py
+++ tools/build_case_topic_pages.py
@@ -23,7 +23,7 @@ ROBOT_HANDOUT_DIR = ROBOT_DIR / "講義"
 ROBOT_WEB_DIR = ROBOT_DIR / "無人車網頁開發"
 DOWNLOAD_TOPIC_06_DIR = REPO / "downloads" / "topic-06-bluetooth-car"
 DOWNLOAD_TOPIC_08_DIR = REPO / "downloads" / "topic-08-fan-application"
-ASSET_VERSION = "20260423-navfix"
+ASSET_VERSION = "20260423-navrow"
 
 
 @dataclass(frozen=True)
